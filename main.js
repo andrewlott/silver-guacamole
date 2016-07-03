c39ed9a44d5fb0415514d9b3498a4682947b3906
Andrew Lott <AndrewLott@gmail.com>
2016-07-03 14:17:01 -0700

    first github commit

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var nmspc = null;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/*', function(req, res){
+  console.log(req.originalUrl);
   res.sendFile(__dirname + '/index.html');
 });
 
