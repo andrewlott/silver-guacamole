a5703cfcc14928d31565242b39e9f5709b6c99c1
Andrew Lott <AndrewLott@gmail.com>
2016-07-03 17:17:08 -0700

    adding rooms, cleaning up code

--- main.js
+++ main.js
@@ -2,29 +2,29 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var nmspc = null;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/*', function(req, res){
-  console.log(req.originalUrl);
+  nmspc = req.originalUrl;
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
   socket.on('vote', function(msg){
-    io.emit('vote', msg);
-    console.log('vote for: ' + msg);
+    var room = msg['room'];
+    var vote = msg['vote'];
+    io.to(room).emit('vote', vote);
   });
   socket.on('reset', function(msg){
-    io.emit('reset', msg);
-    console.log('reset!');
+    var room = msg['room'];
+    var reset = msg['reset'];
+    io.to(room).emit('reset', reset);
   });
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+
   });
 });
 
