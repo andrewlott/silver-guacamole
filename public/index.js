2cc8e4d65bb77a0482604c5e7585e44e35e5303a
Andrew Lott <AndrewLott@gmail.com>
2016-07-05 02:23:36 -0700

    adding bar graph courtesy of d3 :)

--- public/index.js
+++ public/index.js
@@ -11,11 +11,11 @@ var fibonacciVotes = [];
 var average = 0;
 var votes = 0;
 var currentVoteIndex = -1;
-if (!isMobile) {
+//if (!isMobile) {
     setupMain();
-} else {
+//} else {
     setupMobile();
-}
+//}
 
 function reset() {
     votes = 0;
@@ -43,8 +43,10 @@ function setupMain() {
     });
     
     $('body').append('<div id="main">' + average + '</div>');
+    $('body').append('<div id="bar"></div>');
     $('body').append('<div id="reset-button"><button class="reset">Reset</button></div>');
     $('body').append('<div id="source"><a href="https://github.com/andrewlott/silver-guacamole">source</a></div>');
+    setupBar();
     $('body').append('<div id="members"></div>');
     $("button.reset").each(function(index) {
 	$(this).click(function() {
@@ -83,6 +85,70 @@ function setupMobile() {
     });
 }
 
+function setupBar() {
+    $('svg').remove();
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 500 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
+    
+    var x = d3.scale.ordinal()
+	.rangeRoundBands([0, width], .1);
+    
+    var y = d3.scale.linear()
+	.range([height, 0]);
+    
+    var xAxis = d3.svg.axis()
+	.scale(x)
+	.orient("bottom");
+    
+    var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient("left")
+	.ticks(10, "%");
+    
+    var svg = d3.select("#bar").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    var data = [];
+    for (var i = 0; i < fibonacci.length; i++) {
+	data[i] = {letter: fibonacci[i], frequency : fibonacciVotes[i] || 0};
+    }
+	
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+    
+    svg.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(xAxis);
+    
+    svg.append("g")
+	.attr("class", "y axis")
+	.call(yAxis)
+	.append("text")
+	.attr("transform", "rotate(-90)")
+	.attr("y", 6)
+	.attr("dy", ".71em")
+	.style("text-anchor", "end");
+    
+    svg.selectAll(".bar")
+	.data(data)
+	.enter().append("rect")
+	.attr("class", "bar")
+	.attr("x", function(d) { return x(d.letter); })
+	.attr("width", x.rangeBand())
+	.attr("y", function(d) { return y(d.frequency); })
+	.attr("height", function(d) { return height - y(d.frequency); });
+}
+
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
+
 function recalculateWinner() {
     var maxIndex = 0;
     var maxVotes = 0;
@@ -107,6 +173,7 @@ function recalculateWinner() {
     */
     
     $('#main').text(fibonacci[maxIndex]);
+    setupBar();
 }
 
 // Socket.io functions
